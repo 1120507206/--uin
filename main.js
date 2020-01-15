11ee594f75421f7a8f60ae94fe202e61829c039a
1120507206 <52398948+1120507206@users.noreply.github.com>
2020-01-15 11:43:42 +0800

    第一天

---
+++ main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import './static/111/font/iconfont.css'
+import Json from './Json'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
